6bd91623134bd52faa85cd099b0b4c80541c1246
Nabin Maharjan <maharjan.nabin85@gmail.com>
2024-06-30 12:03:14 +0545

    build(modal): pop up modal for user detail clicked on view button

--- client/main.js
+++ client/main.js
@@ -1,21 +1,34 @@
-$(document).ready(function () {
+$(document).ready(function() {
+
   const users = [
-    { username: "Ram Thapa", email: "ram123@gmail.com" },
-    { username: "Sita Gurung", email: "sitagrg@gmail.com" },
-    { username: "Shyam Shrestha", email: "shyamShrestha@gmail.com" },
+    { username: 'Ram Thapa', email: 'ram@gmail.com' },
+    { username: 'Shyam Karki', email: 'shyam@gmail.com' }
   ];
 
-  const $tableBody = $("#userTable tbody");
+  const $tableBody = $('#userTable tbody');
 
+  //append user to the table
   users.forEach((user, index) => {
     const row = `
-        <tr>
-          <th scope="row">${index + 1}</th>
-          <td>${user.username}</td>
-          <td>${user.email}</td>
-          <td><button class="btn btn-primary">View</button></td>
-        </tr>
-      `;
+      <tr>
+        <th scope="row">${index + 1}</th>
+        <td>${user.username}</td>
+        <td>${user.email}</td>
+        <td><button class="btn btn-primary view-details" data-index="${index}">View</button></td>
+      </tr>
+    `;
     $tableBody.append(row);
+  });
+
+  // For modal
+  $tableBody.on('click', '.view-details', function() {
+    const userIndex = $(this).data('index');
+    const user = users[userIndex];
+
+    $('#modalUsername').text(user.username);
+    $('#modalEmail').text(user.email);
+
+    const modal = new bootstrap.Modal(document.getElementById('userModal'));
+    modal.show();
   });
 });
